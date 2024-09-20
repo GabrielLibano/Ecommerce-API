0bf2bd719d4651627b278b2213101cfd35cb4fdb
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-20 19:50:11 -0300

    adicionando padrao de camadas

--- src/main/java/br/com/gabriellibano/ecommerce/repository/ProdutoRepository.java
+++ src/main/java/br/com/gabriellibano/ecommerce/repository/ProdutoRepository.java
@@ -1,4 +1,4 @@
-package br.com.gabriellibano.repository;
+package br.com.gabriellibano.ecommerce.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 import br.com.gabriellibano.ecommerce.model.Produto;
 
 @Repository
-public interface ProdutoRepository extends JpaRepository<Produto, Long>{
-	
+public interface ProdutoRepository 
+	extends JpaRepository<Produto, Long> {
 }
