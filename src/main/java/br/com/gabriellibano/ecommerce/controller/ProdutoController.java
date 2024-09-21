0e420a578685635f547f93409d452226fd9f6045
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-20 22:26:01 -0300

    Parametrizando codigo de retorno HTTP

--- src/main/java/br/com/gabriellibano/ecommerce/controller/ProdutoController.java
+++ src/main/java/br/com/gabriellibano/ecommerce/controller/ProdutoController.java
@@ -2,9 +2,9 @@ package br.com.gabriellibano.ecommerce.controller;
 
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.gabriellibano.ecommerce.dtos.ProdutoRequestCreateDto;
 import br.com.gabriellibano.ecommerce.dtos.ProdutoRequestUpdateDto;
 import br.com.gabriellibano.ecommerce.dtos.ProdutoResponseDto;
-import br.com.gabriellibano.ecommerce.model.Produto;
 import br.com.gabriellibano.ecommerce.service.ProdutoService;
 
 @RestController
@@ -28,11 +27,13 @@ public class ProdutoController {
     private ProdutoService produtoService;
 
     @GetMapping
-    public List<ProdutoResponseDto> list() {
-        return produtoService.list()
+    public ResponseEntity<List<ProdutoResponseDto>> list() {
+        List<ProdutoResponseDto> dto = produtoService.list()
         		.stream()
         		.map(e -> new ProdutoResponseDto().toDto(e))
         		.toList();
+        
+        return ResponseEntity.ok().body(dto);
     }
 
     @PostMapping
