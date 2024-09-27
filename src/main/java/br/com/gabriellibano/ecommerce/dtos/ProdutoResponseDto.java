c6b1e8af61372179eb7e21310d24482b93bdf9a7
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-27 19:18:24 -0300

    Implements ProdutoResponseDTO toDto

--- src/main/java/br/com/gabriellibano/ecommerce/dtos/ProdutoResponseDto.java
+++ src/main/java/br/com/gabriellibano/ecommerce/dtos/ProdutoResponseDto.java
@@ -1,8 +1,21 @@
 package br.com.gabriellibano.ecommerce.dtos;
+import org.modelmapper.ModelMapper;
 
+import br.com.gabriellibano.ecommerce.model.Produto;
 public class ProdutoResponseDto {
     private Long id;
 	private String nome;
+	private static final ModelMapper modelMapper = new ModelMapper();
+	
+	public ProdutoResponseDto toDto(Produto produto) {
+        return modelMapper.map(produto, ProdutoResponseDto.class);
+
+        // ProdutoResponseDto result = new ProdutoResponseDto();
+        // result.setId(produto.getId());
+        // result.setNome(produto.getNome());
+
+        // return result;
+    }
 
     public Long getId() {
         return id;
